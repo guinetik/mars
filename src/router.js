e4db30ef20f7934168d63fc4796ddd1111e2ee6e
guinetik <guinetik@gmail.com>
2026-03-26 16:07:44 -0300

    feat: add /explore/:landmarkId route for terrain exploration

--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import GlobeView from './views/GlobeView.vue'
 import AboutView from './views/AboutView.vue'
 import GisView from './views/GisView.vue'
+import ExploreView from './views/ExploreView.vue'
 
 const routes = [
   { path: '/', name: 'Globe', component: GlobeView },
   { path: '/about', name: 'About', component: AboutView },
   { path: '/gis', name: 'Mars GIS', component: GisView },
+  { path: '/explore/:landmarkId', name: 'Explore', component: ExploreView },
 ]
 
 export default createRouter({
